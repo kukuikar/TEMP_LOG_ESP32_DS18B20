fe9c0a6cdaa990780576c30ac29e918c34b8abb3
kukuikar <denis@perepetsky.ru>
2024-02-29 14:56:04 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,14 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is connected to GPIO15
-#define ONE_WIRE_BUS 18
-// Setup a oneWire instance to communicate with a OneWire device
+#define ONE_WIRE_BUS 12 //wemos
 OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
 
-DeviceAddress sensor1 = {0x28, 0xDF, 0x2F, 0x57, 0x4, 0xE1, 0x3C, 0x5};
-DeviceAddress sensor2 = {0x28, 0x4D, 0x3E, 0x57, 0x4, 0xE1, 0x3C, 0x6B};
-DeviceAddress sensor3 = {0x28, 0xD4, 0x17, 0x57, 0x4, 0xE1, 0x3C, 0x47};
+DeviceAddress TS1 = {0x28, 0xDF, 0x2F, 0x57, 0x4, 0xE1, 0x3C, 0x5};
+DeviceAddress TS2 = {0x28, 0x4D, 0x3E, 0x57, 0x4, 0xE1, 0x3C, 0x6B};
+DeviceAddress TS3 = {0x28, 0xD4, 0x17, 0x57, 0x4, 0xE1, 0x3C, 0x47};
+DeviceAddress TS4 = {0x28, 0xD4, 0x17, 0x57, 0x4, 0xE1, 0x3C, 0x47};/////////////////////////////
 
 void setup(void)
 {
@@ -22,29 +20,17 @@ void setup(void)
 
 void loop(void)
 {
-  //Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  //Serial.println("DONE");
-
-  //Serial.print("Sensor 1(*C): ");
-  //Serial.println(sensors.getTempC(sensor1));
-  //Serial.print(" Sensor 1(*F): ");
-  //Serial.println(sensors.getTempF(sensor1));
-
-  //Serial.print("Sensor 2(*C): ");
-  //Serial.println(sensors.getTempC(sensor2));
-  //Serial.print(" Sensor 2(*F): ");
-  //Serial.println(sensors.getTempF(sensor2));
-
-  //Serial.print("Sensor 3(*C): ");
-  //Serial.println(sensors.getTempC(sensor3));
-  //Serial.print(" Sensor 3(*F): ");
-  //Serial.println(sensors.getTempF(sensor3));
-  float t1 = sensors.getTempC(sensor1);
-  float t2 = sensors.getTempC(sensor2);
-  float t3 = sensors.getTempC(sensor3);
-  Serial.printf("%3.1f\t%3.1f\t%3.1f \n", t1, t2, t3);
-  delay(1000);
+  static uint32_t tmr = millis();
+  if (millis() - tmr > 1000)
+  {
+    tmr = millis();
+    sensors.requestTemperatures();
+    float t1 = sensors.getTempC(TS1);
+    float t2 = sensors.getTempC(TS2);
+    float t3 = sensors.getTempC(TS3);
+    float t4 = sensors.getTempC(TS4);
+    Serial.printf("%3.1f\t%3.1f\t%3.1f\t%3.1f\n", t1, t2, t3, t4);
+  }
 }
 
 /*
