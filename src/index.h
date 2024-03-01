9677c35e6144be99eddb3815a0dbfcea6dac59b5
kukuikar <denis@perepetsky.ru>
2024-03-01 08:51:09 +0300

    main

---
+++ src/index.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+<html>
+<head>
+    <title>TemLOG ESP WIFI</title>
+</head>
+
+<body>
+    <h1>TEMP LOGGER</h1>
+    
+</body>
+</html>
+)rawliteral";
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,49 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESPAsyncWebServer.h>
 
-#define ONE_WIRE_BUS 12 //wemos
+
+
+#if defined(ESP32)
+#define ONE_WIRE_BUS 15 //esp-32
+#include <WiFi.h>
+
+#else
+#define ONE_WIRE_BUS D5 //wemos
+#include <ESP8266WiFi.h>
+#endif
+
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+AsyncWebServer server(80);
 
 DeviceAddress TS1 = {0x28, 0xDF, 0x2F, 0x57, 0x4, 0xE1, 0x3C, 0x5};
 DeviceAddress TS2 = {0x28, 0x4D, 0x3E, 0x57, 0x4, 0xE1, 0x3C, 0x6B};
 DeviceAddress TS3 = {0x28, 0xD4, 0x17, 0x57, 0x4, 0xE1, 0x3C, 0x47};
 DeviceAddress TS4 = {0x28, 0xD4, 0x17, 0x57, 0x4, 0xE1, 0x3C, 0x47};/////////////////////////////
 
+#include "index.h"
+
+void indexHandler(AsyncWebServerRequest *request)
+{
+  request->send(200, "text/html", INDEX_HTML);
+  // request->send(SPIFFS, "/index.html", "text/html");
+  // request->send(SPIFFS, "/index.html", String(), false);
+}
+
 void setup(void)
 {
   pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
   Serial.begin(115200);
   sensors.begin();
+
+  server.on("/", HTTP_GET, indexHandler);
+  //server.on("/status", HTTP_GET, statusHandler);
+  //server.on("/action", HTTP_GET, commandHandler);
+  //server.on("/status", HTTP_OPTIONS, preflight);
+
+  server.begin();
 }
 
 void loop(void)
@@ -25,10 +53,12 @@ void loop(void)
   {
     tmr = millis();
     sensors.requestTemperatures();
+    
     float t1 = sensors.getTempC(TS1);
     float t2 = sensors.getTempC(TS2);
     float t3 = sensors.getTempC(TS3);
     float t4 = sensors.getTempC(TS4);
+
     Serial.printf("%3.1f\t%3.1f\t%3.1f\t%3.1f\n", t1, t2, t3, t4);
   }
 }
